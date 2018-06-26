6cb6ac4458622b06790ffe2539060ebf6af3e4ca
Romane <romane71193@gmail.com>
2018-06-25 23:36:22 -0400

    figure sessions

--- hangman_sinatra.rb
+++ hangman_sinatra.rb
@@ -1,9 +1,37 @@
 
 require 'sinatra'
 
+enable :sessions
 
+NUMBER = 'good'
+def go
+@files = File.readlines("five_desk.txt")
+@pick = @files.select { |w| w.size > 5 && w.size < 12 }.sample
+
+@secret_word = @pick
+@secret_word.downcase!
+end
 
 get '/' do
-  
-  'Hello world!'
+
+@session = session[:number] = rand(10)
+
+
+
+erb :index
+end
+
+get '/outplause' do
+@session = session[:number]
+if params[:guess].to_i == session[:number].to_i
+  @message = 'Yes'
+  erb :come
+else
+  @message = 'No'
+  erb :come
+
+end
+# erb :come
+
+
 end
