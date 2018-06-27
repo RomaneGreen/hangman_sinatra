e3601e36cc084b66f6879e5bf7443cdaa40cc3c2
Romane <romane71193@gmail.com>
2018-06-27 18:47:39 -0400

    images

--- hangman_sinatra.rb
+++ hangman_sinatra.rb
@@ -3,42 +3,30 @@ require 'sinatra'
 
 enable :sessions
 
-NUMBER = 'good'
-def go
+get '/' do
+
 @files = File.readlines("five_desk.txt")
 @pick = @files.select { |w| w.size > 5 && w.size < 12 }.sample
-
 @secret_word = @pick
 @secret_word.downcase!
-end
 
-get '/' do
-  @files = File.readlines("five_desk.txt")
-  @pick = @files.select { |w| w.size > 5 && w.size < 12 }.sample
-
-  @secret_word = @pick
-  @secret_word.downcase!
-
-@session = session[:number] = @secret_word
-@@guessesz = session[:guessesz] = 11
-
-
+@session = session[:word] = @secret_word
+@@guessesz = session[:guessesz] = 10
+  @image = "images/10.jpg"
 erb :index
 end
 
-get '/outplause' do
-@session = session[:number]
+get '/output' do
+@session = session[:word]
 if  @session.include?(params[:guess])
-  #session[:number].include?(params[:guess])
-  @message = 'Yes'
-  erb :come
+    @message = 'Correct'
+    @image = "images/"+@@guessesz.to_s+".jpg"
+  erb :result
 else
-  @message = 'No'
+  @message = 'Incorrect'
   @@guessesz -= 1
-  erb :come
-
+  @image = "images/"+@@guessesz.to_s+".jpg"
+  erb :result
 end
-# erb :come
-
 
 end
