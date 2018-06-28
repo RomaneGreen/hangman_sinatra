422640c77a6516e4251bbf578a346a152ca7931d
Romane <romane71193@gmail.com>
2018-06-27 22:48:49 -0400

    gemfile upd

--- hangman_sinatra.rb
+++ hangman_sinatra.rb
@@ -9,7 +9,12 @@ get '/' do
 @pick = @files.select { |w| w.size > 5 && w.size < 12 }.sample
 @secret_word = @pick
 @secret_word.downcase!
+@final_word = @secret_word.gsub(/[a-z]/," _ ")
 
+
+
+
+@final_word = session[:final_word] = @final_word
 @session = session[:word] = @secret_word
 @@guessesz = session[:guessesz] = 10
   @image = "images/10.jpg"
@@ -18,13 +23,27 @@ end
 
 get '/output' do
 @session = session[:word]
-if  @session.include?(params[:guess])
+@final_word = session[:final_word]
+ #@secret_word = session[:secret_word]
+ if  @final_word.all? { |e| e != " _ "  }
+   @message = 'Congrats! You guessed the word!'
+   @answer = @session.index(params[:guess])
+ @final_word[@answer.to_i] = params[:guess]
+  @final_word.sub!(/ _ /,'')
+   @image = "images/"+@@guessesz.to_s+".jpg"
+ erb :result
+
+elsif  @session.include?(params[:guess])
+
     @message = 'Correct'
+    @answer = @session.index(params[:guess])
+  @final_word[@answer.to_i] = params[:guess]
+   @final_word.sub!(/ _ /,'')
 
     @image = "images/"+@@guessesz.to_s+".jpg"
   erb :result
 elsif @@guessesz == 1
-  @message = 'GAME OVER! You lost!'
+  @message = "GAME OVER! You lost! The word was: #{@session}!"
   @@guessesz = 0
   @image = "images/"+@@guessesz.to_s+".jpg"
   erb :result
